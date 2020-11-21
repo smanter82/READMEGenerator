33dc3ba258e13fb5c832bd37eb36ef4a26f61980
Sarah <70615195+smanter82@users.noreply.github.com>
2020-11-21 07:02:02 -0500

    added import links, started questions list, set up function to write readme file

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const axios = require('axios');  
+// const writeFileAsync = util.promisify(fs.writeFile)
+
+//Import generate markdown file
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your file name?',
+        name: 'fileName',  
+    },
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description of your project.',
+        name: 'projectDescription'
+    },
+//add the rest of the questions.
 ];
+inquirer.prompt(questions)
+.then((response) => {
+    // var data = `### ${response.projectTitle}`
+    writeToFile(fileName, data)
+})
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName + '.md', data, {}, (e) => console.log(e));
 }
+
 
 // function to initialize program
 function init() {
